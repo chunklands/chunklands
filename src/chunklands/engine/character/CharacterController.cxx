9f6887c45a586fba0ecea6f63f630a2b3423dc9e
Philipp Renoth <renoth@wildoak.de>
2020-08-28 11:15:02 +0200

    src: fix sideward move collision

--- src/chunklands/engine/character/CharacterController.cxx
+++ src/chunklands/engine/character/CharacterController.cxx
@@ -10,7 +10,24 @@ constexpr float MAX_PITCH = +LOOK_RADIANS / 2.0;
 
 void CharacterController::MoveAndLook(const glm::vec2& forward_right, const glm::vec2& look_delta)
 {
-    {
+
+    const bool will_change_look = glm::any(glm::notEqual(look_delta, glm::vec2(0, 0)));
+    const bool will_move = glm::any(glm::notEqual(forward_right, glm::vec2(0, 0)));
+
+    if (will_change_look) {
+        glm::vec2 look = camera_.GetLook() + look_delta;
+
+        // yaw modulo
+        look.x = std::fmod(look.x, float(2.f * M_PI));
+
+        // pitch range
+        look.y = std::max(MIN_PITCH, look.y);
+        look.y = std::min(MAX_PITCH, look.y);
+
+        camera_.SetLook(look);
+    }
+
+    if (will_move) {
         const float length = glm::length(forward_right);
         float forward = forward_right[0];
         float right = forward_right[1];
@@ -34,24 +51,20 @@ void CharacterController::MoveAndLook(const glm::vec2& forward_right, const glm:
             sin_pitch * forward,
             cos_pitch * cos_yaw * forward + sin_yaw * right);
 
-        auto response = movement_controller_.CalculateMovement(engine_chunk_data_, camera_.GetEye(), std::move(move));
-        camera_.SetEye(response.new_camera_pos);
+        constexpr bool collision = true;
+
+        if (collision) {
+            auto response = movement_controller_.CalculateMovement(engine_chunk_data_, camera_.GetEye(), move + glm::vec3(0, 0, 0) // fix -0.0 values
+            );
+            camera_.SetEye(response.new_camera_pos);
+        } else {
+            camera_.SetEye(camera_.GetEye() + move);
+        }
     }
 
-    {
-        glm::vec2 look = camera_.GetLook() + look_delta;
-
-        // yaw modulo
-        look.x = std::fmod(look.x, float(2.f * M_PI));
-
-        // pitch range
-        look.y = std::max(MIN_PITCH, look.y);
-        look.y = std::min(MAX_PITCH, look.y);
-
-        camera_.SetLook(look);
+    if (will_change_look || will_move) {
+        engine_render_data_.pointing_block = movement_controller_.PointingBlock(engine_chunk_data_, math::fLine3(camera_.GetEye(), camera_.GetViewDirection() * 4.f));
     }
-
-    engine_render_data_.pointing_block = movement_controller_.PointingBlock(engine_chunk_data_, math::fLine3(camera_.GetEye(), camera_.GetViewDirection() * 2.f));
 }
 
 } // namespace chunklands::engine::character
